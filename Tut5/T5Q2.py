f50f6c161d22298ae661654062eb3cd839614ce7
khvmaths <32212455+khvmaths@users.noreply.github.com>
2018-11-01 10:47:05 +0800

    Update T5Q2.py

--- Tut5/T5Q2.py
+++ Tut5/T5Q2.py
@@ -1,11 +1,5 @@
 from functools import reduce
 
-def g(m,n):
-    sum=1
-    for i in range(1,n+1):
-        sum*=i
-    return sum
-
 n=5
-f=reduce(g,range(n+1) if n>0 else range (n+2))
+f=reduce(lambda x,y:x*y, range(1,n+1) )
 print(f)
