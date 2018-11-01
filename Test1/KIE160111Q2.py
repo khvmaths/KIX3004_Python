f74e7dfe812163b68dfa20eac0622b92c75d6836
khvmaths <32212455+khvmaths@users.noreply.github.com>
2018-11-01 11:51:35 +0800

    Update KIE160111Q2.py

--- Test1/KIE160111Q2.py
+++ Test1/KIE160111Q2.py
@@ -5,7 +5,8 @@ for _ in range(4):
     c.append(float(input('Enter the current : ')))
     r.append(float(input('Enter the rpm : ')))
     
-print('Voltage\tCurrent\tRPM\tRange')
+print('Voltage\tCurrent\t  RPM\t\t Range')
 for i in range(4):
     q=float((v[i]*1.2)+(c[i]*0.5)+(r[i]*0.2))
-    print(v[i],c[i],v[i],q,sep="\t")
+    print(v[i],c[i],r[i],sep="\t ",end="")
+    print('\t\t',q)
