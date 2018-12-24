9439bdf9da83ed2f75b54078650e0442a856cd9a
khvmaths <32212455+khvmaths@users.noreply.github.com>
2018-12-24 20:00:52 +0800

    Update T2Q1.py

--- Tut2/T2Q1.py
+++ Tut2/T2Q1.py
@@ -1,12 +1,12 @@
-import math
+from numpy import arctan
 
 print('***Complex Number in Polar Form ***')
 a=complex(input('Please enter a complex number: '))
-m=math.sqrt(a.real**2+a.imag**2)
+m=(a.real**2+a.imag**2)**0.5
 if a.real>0:
-    p=math.atan(a.imag/a.real)
+    p=arctan(a.imag/a.real)
 else:
-    p=math.atan(a.imag/a.real)+math.pi
+    p=arctan(a.imag/a.real)+math.pi
 print('The complex number in polar form ')
 print('m = %f\np = %f' %(m,p))
 print('*** End ***')
